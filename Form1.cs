0f4695584bc0c23fb72fd1a177b489d5c36e3d0a
wendam <wendam@laposte.net>
2016-05-17 23:26:19 +0200

    Ajout bouton

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace Certification
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Test");
+        }
     }
 }
